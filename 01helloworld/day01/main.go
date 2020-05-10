ea2d1b9c79b38812c22dfd48662f83eda2ecec5e
e <myprohecy@163.com>
2020-05-11 00:52:24 +0800

    Print

--- 01helloworld/day01/main.go
+++ 01helloworld/day01/main.go
@@ -7,6 +7,14 @@ import "fmt"
 
 
 func main()  {
-	
+
+	var (
+		name string
+		age int
+	)
+	name = "jack"
+	age = 12
 	fmt.Println("Hello World!!!")
+	fmt.Println("name %s",name)
+	fmt.Println("age %d",age)
 }
